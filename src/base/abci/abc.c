3af017652746bf0addbb97b2808ef9c8fd2ceef6
MyskYko <yoyuohlhjl@yahoo.co.jp>
2019-10-18 14:12:29 +0900

    modified for AND of const0 and const0 to be const0 during verification

--- src/base/abci/abc.c
+++ src/base/abci/abc.c
@@ -46112,13 +46112,21 @@ int Abc_CommandAbc9Cspf( Abc_Frame_t * pAbc, int argc, char ** argv )
 	    for ( i = 0; i < nDcPos; i++ )
 	      {
 		vDcPos[i] = nDcPos + nDcPos + i;
-		v = Gia_ManAppendAnd( p00,
-				      Gia_Obj2Lit( p00, Gia_ObjChild0( Gia_ManCo( p00, i ) ) ),
-				      Abc_LitNot( Gia_Obj2Lit( p00, Gia_ObjChild0( Gia_ManCo( p00, i + nDcPos ) ) ) ) );
+		if ( Gia_ObjIsConst0( Gia_ObjChild0( Gia_ManCo( p00, i ) ) ) &&
+		     Gia_ObjIsConst0( Gia_Not( Gia_ObjChild0( Gia_ManCo( p00, i + nDcPos ) ) ) ) )
+		  v = Gia_ManConst0Lit( p00 );
+		else
+		  v = Gia_ManAppendAnd( p00,
+					Gia_Obj2Lit( p00, Gia_ObjChild0( Gia_ManCo( p00, i ) ) ),
+					Abc_LitNot( Gia_Obj2Lit( p00, Gia_ObjChild0( Gia_ManCo( p00, i + nDcPos ) ) ) ) );
 		Gia_ManAppendCo( p00, v );
-		v = Gia_ManAppendAnd( p10,
-				      Gia_Obj2Lit( p10, Gia_ObjChild0( Gia_ManCo( p10, i ) ) ),
-				      Abc_LitNot( Gia_Obj2Lit( p10, Gia_ObjChild0( Gia_ManCo( p10, i + nDcPos ) ) ) ) );
+		if ( Gia_ObjIsConst0( Gia_ObjChild0( Gia_ManCo( p10, i ) ) ) &&
+		     Gia_ObjIsConst0( Gia_Not( Gia_ObjChild0( Gia_ManCo( p10, i + nDcPos ) ) ) ) )
+		  v = Gia_ManConst0Lit( p10 );
+		else
+		  v = Gia_ManAppendAnd( p10,
+					Gia_Obj2Lit( p10, Gia_ObjChild0( Gia_ManCo( p10, i ) ) ),
+					Abc_LitNot( Gia_Obj2Lit( p10, Gia_ObjChild0( Gia_ManCo( p10, i + nDcPos ) ) ) ) );
 		Gia_ManAppendCo( p10, v );
 	      }
 	    p01 = Gia_ManDupCones( p00, vDcPos, nDcPos, 0 );
